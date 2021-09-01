59b74b6ecf62cd6b3fa0c58b9b7b9b1f61bf4f33
benyamin <benyamin@benyamins-MacBook-Air.local>
2021-09-01 20:19:14 +0430

    first structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import Form from './components/form/Form';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import MainTable from './components/mainTable/MainTable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container >
+      <Switch>
+        <Route exact path="/">
+          <Form />
+        </Route>
+        <Route path="/table">
+          <MainTable />
+        </Route>
+        <Route path="/content">
+          <Form />
+        </Route>
+      </Switch>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+background-color: #E5E5E5;
+height: 100vh;
+display: flex;
+justify-content: center;
+align-items: center;
+`
